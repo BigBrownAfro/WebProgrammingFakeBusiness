a6246d90324eec5e184018199007eed834dcaf03
BigBrownAfro <bigbrownafro@gmail.com>
2019-11-04 16:29:45 -0700

    Search bar focuses as intended

--- src/app/title-bar/title-bar.component.ts
+++ src/app/title-bar/title-bar.component.ts
@@ -1,34 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-title-bar',
   templateUrl: './title-bar.component.html',
   styleUrls: ['./title-bar.component.css']
 })
+
 export class TitleBarComponent implements OnInit {
   title:String;
 
   constructor() { 
-    this.title = "Citio";
-
-    /* <button (click)="onClickMe()">Click me!</button> */
-    
+    this.title = "Bideo Gaming";
   }
 
   ngOnInit() {
-    var searchInput = document.getElementById('search-input')
-    if(!searchInput){
-      console.log("Search Input doesn't exist")
-    }
 
-    var leftSideSearchBar = document.getElementById("left-search");
-    if(leftSideSearchBar){
-      leftSideSearchBar.addEventListener('click', () => {
-        searchInput.focus();
-      })
-    }else{
-      console.log("LeftSideSearchBar doesn't exist");
-    }
+  }
+
+  onClickLeftSearch(inputElement:HTMLInputElement){
+    inputElement.focus();
   }
 
 }
