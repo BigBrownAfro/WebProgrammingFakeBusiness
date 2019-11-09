3aae676a8d5f9d97935b5f15993eb71a48ea1f53
BigBrownAfro <bigbrownafro@gmail.com>
2019-11-08 20:49:26 -0700

    Page darkens and is untouchable when sidebar is open. Put a list in the sidebar. Changed some styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { TitleBarComponent } from './title-bar/title-bar.component';
+import { Component, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +6,20 @@ import { TitleBarComponent } from './title-bar/title-bar.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //pageContent: HTMLDivElement;
+  //@ViewChildren('pageContent') pageContent: HTMLDivElement;
   private _opened = false;
+  brightness = 1;
 
   private _toggleSidebar(){
+    //@ViewChildren('pageContent') pageContent: HTMLDivElement;
+    const pageContent = document.getElementById("pageContent");
     this._opened = !this._opened;
+    if(this._opened){
+      pageContent.style.filter = "brightness(.5)";
+      pageContent.style.cursor = "default";
+    }else{
+      pageContent.style.filter = "brightness(1)";
+    }
   }
 }
