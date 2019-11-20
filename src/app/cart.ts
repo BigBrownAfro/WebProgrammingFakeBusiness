2e17b550007f8092ce4217181c5680e6fbf596a9
BigBrownAfro <bigbrownafro@gmail.com>
2019-11-20 14:52:45 -0700

    Quantities for cart items are stored. Need to update cart size.

--- src/app/cart.ts
+++ src/app/cart.ts
@@ -2,18 +2,32 @@ import { Item } from './item';
 
 export class Cart{
     items:Item[];
+    quantities:number[];
     size:number;
 
     constructor(){
         this.items = [];
+        this.quantities = [];
         this.size = 0;
     }
 
     /**
      * Pushes a new item into the list of items and updates the size of the cart
+     * If item is already in the list, quantity associated with item increments by one
      * @param i Item to be added
      */
     public add(i:Item){
-        this.size = this.items.push(i);
+        let index = this.items.indexOf(i)
+        console.log(index);
+
+        if(index === -1){
+            console.log('new item added');
+            
+            this.size = this.items.push(i);
+            this.quantities.push(1);
+        }else{
+            this.quantities[index] += 1;
+            console.log("quantity: " + this.quantities[index]);
+        }
     }
 }
